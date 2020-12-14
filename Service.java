ee8ca03ee78fb49172a948def8147adf80f5802f
Karan Narula <narulaskaran@gmail.com>
2020-12-13 23:10:04 -0800

    reset vscode dependencies

--- Service.java
+++ Service.java
@@ -1,23 +1,8 @@
 import io.javalin.Javalin;
-import static io.javalin.apibuilder.ApiBuilder.*;
 
 public class Service {
     public static void main(String[] args) {
-        Javalin app = Javalin.create(config -> {
-            config.defaultContentType = "application/json";
-            config.addStaticFiles("/public");
-            config.enableCorsForAllOrigins();
-        }).routes(() -> {
-            path("users", () -> {
-                get(UserController::getAll);
-                post(UserController::create);
-                path(":user-id", () -> {
-                    get(UserController::getOne);
-                    patch(UserController::update);
-                    delete(UserController::delete);
-                });
-                ws("events", userController::webSocketEvents);
-            });
-        }).start(port);
+        Javalin app = Javalin.create().start(7000);
+        app.get("/", ctx -> ctx.result("Hello World"));
     }
 }
