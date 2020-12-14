21a9beefb0b57b675f687e62384591c3e3a2fa92
Karan Narula <narulaskaran@gmail.com>
2020-12-13 23:24:27 -0800

    init intellij project

--- Service.java
+++
@@ -1,8 +0,0 @@
-import io.javalin.Javalin;
-
-public class Service {
-    public static void main(String[] args) {
-        Javalin app = Javalin.create().start(7000);
-        app.get("/", ctx -> ctx.result("Hello World"));
-    }
-}
